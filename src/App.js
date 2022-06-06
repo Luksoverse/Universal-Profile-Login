bfd0b8e4ba0f30ac2d474479d8316d122a04418b
B00ste <afteni.danu@gmail.com>
2022-06-07 00:21:57 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import Web3 from 'web3';
+const web3 = new Web3(window.ethereum);
+
 function App() {
+
+  const [account, setAccount] = useState(undefined);
+
+  const connect = async () => {
+    const accounts = await web3.eth.requestAccounts();
+    console.log(accounts);
+    setAccount(accounts[0]);
+  }
+  const getNonce = async (publicAddress) => {
+    console.log(account);
+    console.log(publicAddress);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ publicAddress })
+    };
+    const nonce = await fetch('https://up-auth.herokuapp.com/nonce', requestOptions)
+      .then(response => response.json())
+      .then(data => data["nonce"]);
+    alert(nonce);
+    console.log(nonce);
+    //signNonce(nonce, publicAddress);
+  }
+  const signNonce = async (nonce, publicAddress) => {
+    web3.eth.personal.sign(`${nonce}`, publicAddress, (err, res) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log(res);
+      }
+      /*if (!err) {
+        verifySignature(publicAddress, res);
+      }*/
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='btn-container'>
+      <button onClick={() => connect()} className='btn connect'>Connect</button>
+      <button onClick={() => getNonce(account)} className='btn sign'>Sign</button>
+      <button className='btn verify'>Verify</button>
     </div>
   );
 }
