c335db524fed4f14c09caceb3c88256d2b50b358
B00ste <afteni.danu@gmail.com>
2022-06-07 22:22:00 +0300

    Verifing the signature.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ function App() {
     updated: false,
     nonce: undefined,
     signature: undefined
-  })
-
+  });
   const connect = async () => {
     const accounts = await web3.eth.requestAccounts();
     setAccount(accounts[0]);
@@ -37,12 +36,22 @@ function App() {
     const signature = await signNonce(publicAddress, nonce);
     setData({ updated: true, nonce, signature })
   }
+  const verifySignature = async (publicAddress, signature) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ publicAddress, signature })
+    };
+    const res = await fetch('https://up-auth.herokuapp.com/auth', requestOptions)
+      .then(response => response.json());
+    console.log(res);
+  }
 
   return (
     <div className='btn-container'>
       <button onClick={() => connect()} className='btn connect'>Connect</button>
       <button onClick={() => sign(account)} className='btn sign'>Sign</button>
-      <button className='btn verify'>Verify</button>
+      <button onClick={() => verifySignature(account, data.signature)} className='btn verify'>Verify</button>
       {
         account
         ? <p className='account-data'>Account: {account}</p>
